4d5ac3293239e647694cab348e5241888b41b81d
Anish Pallati <anishp@duck.com>
2025-07-09 18:44:56 -0400

    feat: create placeholder components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+    return (
+        <nav className="p-2 flex justify-between items-center">
+            <h1>CMU Courses</h1>
+            <button type="button">Login</button>
+        </nav>
+    );
+};
