e2b5eb8790d18890b1162da83378521812ec404b
devnmoor <noraayibatarimoor@gmail.com>
2026-03-14 17:44:08 -0400

    bun run dev fixed, just did not have baseline-browser-mapping installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite' // API outdated and should be updated to @tanstack/router-plugin/vite
-// import { test } from ....
+// import { test } from 'vitest'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
